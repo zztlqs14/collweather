28c203c2ae25578db1981679c4e53e958def48d2
zztlqs14 <1531733886@qq.com>
2019-06-03 14:00:25 +0800

    备注

---
+++ collweather/app/src/main/java/com/example/collweather/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.collweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
